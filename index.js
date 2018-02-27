5578615b966198b4fd88c766f35549fb446c95a9
befern <belen.fdez.glez@gmail.com>
2018-02-27 17:23:31 +0100

    Node MongoDB Example 1

---
+++ index.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/conFusion';
+
+MongoClient.connect(url, (err, db) => {
+    
+    assert.equal(err, null);
+
+    console.log('Connected correctly to server');
+
+    const database= db.db('conFusion')
+    const collection = database.collection("dishes");
+    collection.insertOne({"name": "Uthapizza", "description": "test"}, 
+    (err, result) => {
+        assert.equal(err, null);
+
+        console.log("After Insert:\n");
+        console.log(result.ops);
+
+        collection.find({}).toArray((err, docs) => {
+            assert.equal(err, null);
+
+            console.log("Found:\n");
+            console.log(docs);
+
+            database.dropCollection("dishes", (err, result) => {
+                assert.equal(err, null);
+
+                db.close();
+            });
+        });
+    });
+
+});
