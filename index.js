d7ad6efd513c99f536f04cfa52658557ed599617
befern <belen.fdez.glez@gmail.com>
2018-02-27 18:36:08 +0100

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const dboper = require('./operations.js');
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err, db) => {
-    
+MongoClient.connect(url, (err, database) => {
+
+    db = database.db("ConFusion")
     assert.equal(err, null);
 
     console.log('Connected correctly to server');
 
-    const database= db.db('conFusion')
-    const collection = database.collection("dishes");
-    collection.insertOne({"name": "Uthapizza", "description": "test"}, 
-    (err, result) => {
-        assert.equal(err, null);
+    dboper.insertDocument(db, {name: "Vadonut", description: "Test"}, 
+        "dishes", (result) => {
+            console.log("Insert Document: \n", result.ops);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents: \n", docs);
+            
+                dboper.updateDocument(db, {name: "Vadonut"}, 
+                    {description: "Updated Test"}, "dishes",
+                    (result) => {
+                        console.log("Updated Document  \n", result.result);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents: \n", docs);
 
-            console.log("Found:\n");
-            console.log(docs);
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection ", result);
 
-            database.dropCollection("dishes", (err, result) => {
-                assert.equal(err, null);
-
-                db.close();
+                                database.close();
+                            });
+                        });
+                    });
             });
-        });
-    });
 
+        });
 });
